d29937dbca1a46258ff6312f9bdb76887975264c
“XuHuaiAng” <2533694604@qq.com>
2022-11-18 16:31:58 +0800

    Initial commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,61 @@
+<!-- 首页 -->
+<template>
+  <div :style="{backgroundImage:'url(https://xpoet.cn/images/bg.svg)'}">
+    <sg-navbar></sg-navbar>
+    <div class="container">
+      <el-row :gutter="30">
+        <el-col :sm="24" :md="16" style="transition:all .5s ease-out;margin-bottom:30px;">
+          <sg-articlelist></sg-articlelist>
+        </el-col>
+        <el-col :sm="24" :md="8">
+          <sg-rightlist></sg-rightlist>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import header from '../components/header.vue'
+import articlelist from '../components/articlelist.vue'
+import rightlist from '../components/rightlist.vue'
+
+export default {
+  name: 'Home',
+  data() { //选项 / 数据
+    return {}
+  },
+  methods: { //事件处理器
+
+  },
+  components: { //定义组件
+    'sg-navbar': header,
+    'sg-articlelist': articlelist,
+    'sg-rightlist': rightlist,
+  },
+  created() { //生命周期函数
+
+  }
+}
+</script>
+
+<style>
+::-webkit-scrollbar-track {
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 0;
+}
+
+::-webkit-scrollbar {
+  -webkit-appearance: none;
+  width: 6px;
+  height: 4px;
+}
+
+::-webkit-scrollbar-thumb {
+  cursor: pointer;
+  border-radius: 5px;
+  background: #81c995;
+  transition: color 0.2s ease;
+}
+
+</style>
